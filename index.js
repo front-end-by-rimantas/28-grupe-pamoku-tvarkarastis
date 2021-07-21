cbe830d878e4b30a34eebf4fbd3296ee1e57eb46
Rimantas <belauzas@yahoo.com>
2021-07-21 20:28:06 +0300

    Schedule

---
+++ index.js
@@ -0,0 +1,5 @@
+// čia pateikti visus required/import metodus
+// susikurti pavyzdinius objektus iš class
+// parodyti, jog sukurti metodai tikrai veikia, t.y.
+// prieš tai suvesta informacija turi įtaką likusiems
+// kviečiamiems metodams
